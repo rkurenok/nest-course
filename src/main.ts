82394c63d594387eb7079bc2f516df6ceac44faf
rkurenok <48415490+rkurenok@users.noreply.github.com>
2023-04-11 09:43:23 +0300

    added nest project

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder } from "@nestjs/swagger";
+import { SwaggerModule } from "@nestjs/swagger/dist";
+import { AppModule } from "./app.module";
+
+async function start() {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('Урок по продвинотому BACKEND')
+        .setDescription('Документация REST API')
+        .setVersion('1.0.0')
+        .addTag('Romchik')
+        .build()
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/api/docs', app, document);
+
+    await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+}
+
+start()
